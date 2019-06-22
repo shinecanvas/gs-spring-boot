9f31fd5b5830d553d02b108f523ec6c94a0a3679
shinecanvas <wanxiaruge@qq.com>
2019-06-22 20:00:09 +0800

    springboot 2.2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot 2:");
+            System.out.println("Let's inspect the beans provided by Spring Boot 2.2:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
